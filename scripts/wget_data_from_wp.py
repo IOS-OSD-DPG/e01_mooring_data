f39a38a0ec56355bf952f6c87681bbf83d9770df
hhourston <50595197+hhourston@users.noreply.github.com>
2023-10-19 11:28:26 -0700

    add script for downloading files using wget

---
+++ scripts/wget_data_from_wp.py
@@ -0,0 +1,19 @@
+import wget
+import numpy as np
+from tqdm import trange
+# from Samantha Huntington
+
+
+def get_files(station: str):
+    """wget download for waterproperties files"""
+
+    path = f"E:\\charles\\mooring_data_page\\{station.lower()}\\"
+    address = np.genfromtxt(path + f"wget_file_download_list_{station.lower()}.csv", dtype=str)
+
+    for i in trange(address.size):
+        wget.download('https://' + address[i], out=path + 'ios_shell_data\\')
+    return
+
+
+get_files('chat3')
+get_files('juan2')
